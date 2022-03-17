445525986046fefc1570492a2c1c39845c44c2c4
Kevin Quach <kevinquach2@yahoo.com>
2022-03-17 17:02:27 -0500

    Created operate function and set up basic HTML structure

--- js-file.js
+++ js-file.js
@@ -1,15 +1,23 @@
-const add = function(a,b) {
-    return a+b;
+const add = function(num1, num2) {
+    return num1+num2;
 }  
 
-const subtract = function(a,b) {
-    return a-b;
+const subtract = function(num1, num2) {
+    return num1-num2;
 }
 
-const multiply = function(a,b) {
-    return a*b;
+const multiply = function(num1, num2) {
+    return num1*num2;
 }
 
-const divide = function(a,b) {
-    return a/b;
+const divide = function(num1, num2) {
+    return num1/num2;
 }
+
+const operate = function(operator, num1, num2) {
+    if (operator == 'add') return add(num1, num2);
+    else if (operator == 'subtract') return subtract(num1, num2);
+    else if (operator == 'multiply') return multiply(num1, num2);
+    else return divide(num1, num2);
+}
+
